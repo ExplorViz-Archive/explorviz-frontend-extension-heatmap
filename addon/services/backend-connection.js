baaec628fd5ab45c3ef3399cc6937f0814a1674c
Tim-Niklas Reck <stu114743@mail.uni-kiel.de>
2020-01-29 14:52:37 +0100

    updated master

--- addon/services/backend-connection.js
+++ addon/services/backend-connection.js
@@ -1,7 +1,6 @@
 import Service, {inject as service} from '@ember/service';
 import Evented from '@ember/object/evented';
 
-
 export default Service.extend(Evented, {
   websockets: service(),
 
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 'use strict';
-
 module.exports = {
-  name: 'explorviz-frontend-extension-heatmap',
-
-  isDevelopingAddon() {
-    return true;
-  }
-
-  included: function(app) {
-    this._super.included.apply(this, arguments);    
-    app.import('vendor/style.css');
-  }
+    name: 'explorviz-frontend-extension-heatmap',
+    isDevelopingAddon: function () {
+        return true;
+    },
+    included: function (app) {
+        this._super.included.apply(this, arguments);
+        app["import"]('vendor/style.css');
+    }
 };
